f57ff5d2592b963f8a55d3ab0fde1c8cc86b9946
MDTECH508 <goodfreefire509@gmail.com>
2026-05-16 08:22:59 -0400

    Refactor Video component to MoviePlayer with controls

--- src/routes/Player/Video/Video.js
+++ src/routes/Player/Video/Video.js
@@ -1,24 +1,68 @@
-// Copyright (C) 2017-2023 Smart code 203358507
+import React, { useState } from 'react';
+import VideoControls from 'stremio/components/VideoControls';
+import { EnhancedMetaItem } from 'stremio/components/EnhancedMetaItem';
 
-const React = require('react');
-const PropTypes = require('prop-types');
-const classnames = require('classnames');
-const styles = require('./styles');
+const MoviePlayer = ({ movie }) => {
+    const [isPlaying, setIsPlaying] = useState(false);
+    const [currentTime, setCurrentTime] = useState(0);
+    const [quality, setQuality] = useState('720p');
+    const [subtitle, setSubtitle] = useState('en');
 
-const Video = React.forwardRef(({ className, onClick, onDoubleClick }, ref) => {
     return (
-        <div className={classnames(className, styles['video-container'])} onClick={onClick} onDoubleClick={onDoubleClick}>
-            <div ref={ref} className={styles['video']} />
+        <div style={{ width: '100%', maxWidth: '1200px', margin: '0 auto' }}>
+            {/* Movie Info */}
+            <EnhancedMetaItem
+                name={movie.title}
+                genres={movie.genres}
+                quality={quality}
+                poster={movie.poster}
+                imdbScore={movie.imdbScore}
+                onPlayClick={() => setIsPlaying(true)}
+            />
+
+            {/* Video Player Container */}
+            <div style={{
+                position: 'relative',
+                width: '100%',
+                paddingBottom: '56.25%',
+                backgroundColor: '#000',
+                borderRadius: '0.5rem',
+                overflow: 'hidden',
+                marginTop: '1rem'
+            }}>
+                <video
+                    style={{
+                        position: 'absolute',
+                        top: 0,
+                        left: 0,
+                        width: '100%',
+                        height: '100%'
+                    }}
+                    playing={isPlaying}
+                    src={movie.videoUrl}
+                />
+
+                {/* Player Controls */}
+                <VideoControls
+                    isPlaying={isPlaying}
+                    duration={movie.duration}
+                    currentTime={currentTime}
+                    quality={quality}
+                    subtitle={subtitle}
+                    availableSubtitles={[
+                        { value: 'en', label: 'English' },
+                        { value: 'es', label: 'Español' },
+                        { value: 'ht', label: 'Kreyòl' }
+                    ]}
+                    onPlayPauseClick={() => setIsPlaying(!isPlaying)}
+                    onQualityChange={setQuality}
+                    onSubtitleChange={setSubtitle}
+                    onFullscreenClick={() => {/* Handle fullscreen */}}
+                    onTimeChange={setCurrentTime}
+                />
+            </div>
         </div>
     );
-});
-
-Video.displayName = 'Video';
-
-Video.propTypes = {
-    className: PropTypes.string,
-    onClick: PropTypes.func,
-    onDoubleClick: PropTypes.func,
 };
 
-module.exports = Video;
+export default MoviePlayer;
